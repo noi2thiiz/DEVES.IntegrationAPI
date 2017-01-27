30f88a019a6811b422a064e7ecbe7c7346e6df13
NuchPFC <suwathidak@primefactorconsulting.com>
2017-01-27 19:09:54 +0700

    Added projects

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DEVES.IntegrationAPI.WebApi.Startup))]
+
+namespace DEVES.IntegrationAPI.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
