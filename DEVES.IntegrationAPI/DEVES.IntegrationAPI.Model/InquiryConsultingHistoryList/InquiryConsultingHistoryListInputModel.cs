f521212658cf6734b0151355294642185e92ee08
NuchPFC <suwathidak@primefactorconsulting.com>
2017-01-31 19:39:29 +0700

    Updated model

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/InquiryConsultingHistoryList/InquiryConsultingHistoryListInputModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/InquiryConsultingHistoryList/InquiryConsultingHistoryListInputModel.cs
@@ -7,8 +7,26 @@ using Newtonsoft.Json;
 
 namespace DEVES.IntegrationAPI.Model.InquiryConsultingHistoryList
 {
+
     public class InquiryConsultingHistoryListInputModel
     {
         public string code { get; set; }
+        public string message { get; set; }
+        public string description { get; set; }
+        public string transactionId { get; set; }
+        public DateTime transactionDateTime { get; set; }
+        public InquiryConsultingHistoryListDataInputModel[] data { get; set; }
+    }
+
+    public class InquiryConsultingHistoryListDataInputModel
+    {
+        public string ticketNo { get; set; }
+        public string claimNotiNo { get; set; }
+        public string claimNo { get; set; }
+        public string crmChannel { get; set; }
+        public string crmNote { get; set; }
+        public DateTime crmCreatedOn { get; set; }
+        public string crmCreatedBy { get; set; }
     }
 }
+
