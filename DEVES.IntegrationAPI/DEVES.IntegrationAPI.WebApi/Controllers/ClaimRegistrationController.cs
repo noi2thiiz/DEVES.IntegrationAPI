cd7c65f13b29a948267a612603db5391170955c0
np_jaa <mini.nh@live.com>
2017-02-17 13:26:45 +0700

    Update <Before adding web service> - Add "MVS" to premiumClass

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/ClaimRegistrationController.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/ClaimRegistrationController.cs
@@ -87,7 +87,7 @@ namespace DEVES.IntegrationAPI.WebApi.Controllers
             cr.claimHeader = new LocusClaimheaderModel();
             // claimHeader
             // Recently Add 
-            cr.claimHeader.premiumClass = "";
+            cr.claimHeader.premiumClass = "MVS";
             cr.claimHeader.teamCd = "";
             cr.claimHeader.claimStatus = "";
             // Recently Add 
