7df9f25d89d5136aed26f1debab26cbd0f5f721a
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-03-15 18:08:47 +0700

    Update - AccidentPrilim -> Model with Mapping variable - RVP -> Model with Mapping variable
    To Do
    - Mapping these variables in field and test

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/AccidentPrilimSurveyorReport/AccidentPrilimSurveyorReportInputModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/AccidentPrilimSurveyorReport/AccidentPrilimSurveyorReportInputModel.cs
@@ -9,35 +9,95 @@ namespace DEVES.IntegrationAPI.Model.AccidentPrilimSurveyorReport
 {
     public class AccidentPrilimSurveyorReportInputModel
     {
-        public string token { get; set; }
         public string ticketNo { get; set; }
         public string claimNotiNo { get; set; }
-        public string reportAccidentResultDate { get; set; }
-        public string accidentLefalResult { get; set; }
+        public string eventId { get; set; }
+        public string caseOwnerCode { get; set; }
+        public string caseOwnerFullName { get; set; }
+        public DateTime reportAccidentResultDate { get; set; }
+        public EventDetailInfoModel eventDetailInfo { get; set; }
+        public List<PartiesInfoModel> partiesInfo { get; set; }
+        public List<ClaimDetailInfoModel> claimDetailInfo { get; set; }
+        public List<ClaimDetailPartiesInfoModel> claimDetailPartiesInfo { get; set; }
+    }
+
+    public class EventDetailInfoModel
+    {
+        public string accidentOn { get; set; }
+        public string accidentLatitude { get; set; }
+        public string accidentLongitude { get; set; }
+        public string accidentPlace { get; set; }
+        public string accidentNatureDesc { get; set; }
+        public string accidentRemark { get; set; }
+        public string accidentLegalResult { get; set; }
         public string policeStation { get; set; }
         public string policeRecordId { get; set; }
-        public DateTime policeRecordDate { get; set; }
+        public string policeRecordDate { get; set; }
         public string policeBailFlag { get; set; }
-        public string demageOfPolicyOwnerCar { get; set; }
         public int numOfTowTruck { get; set; }
-        public string nameOfTowCompany { get; set; }
-        public string detailOfTowEvent { get; set; }
         public int numOfAccidentInjury { get; set; }
-        public string detailOfAccidentInjury { get; set; }
         public int numOfDeath { get; set; }
-        public string detailOfDeath { get; set; }
-        public string caseOwnercode { get; set; }
-        public string caseOwnerFullname { get; set; }
-        public string accidentPartyInfo { get; set; }
-        public string accidentPartyFullname { get; set; }
-        public string accidentPartyPhone { get; set; }
-        public string accidentPartyCarPlateNumber { get; set; }
-        public string accidentPartyCarModel { get; set; }
-        public string accidentPartyInsuredFlag { get; set; }
-        public string accidentPartyInsuranceCompany { get; set; }
-        public string accidentPartyPolicyType { get; set; }
-        public string accidentPartyPolicyNumber { get; set; }
-        public DateTime accidentPartyPolicyExpDate { get; set; }
-        public string demageOfPartyCar { get; set; }
+        public int deductibleFee { get; set; }
+        public int excessFee { get; set; }
+        public string totalEvent { get; set; }
+        public string iSurveyCreatedDate { get; set; }
+        public string iSurveyModifiedDate { get; set; }
+        public string iSurveyIsDeleted { get; set; }
+        public string iSurveyIsDeletedDate { get; set; }
+        public int numOfAccidentParty { get; set; }
     }
+
+    public class PartiesInfoModel
+    {
+        public string partiesEventId { get; set; }
+        public string partiesEventItem { get; set; }
+        public string partiesId { get; set; }
+        public string partiesFullname { get; set; }
+        public int partiesType { get; set; }
+        public string partiesCarPlateNo { get; set; }
+        public string partiesCarPlateProv { get; set; }
+        public string partiesCarBrand { get; set; }
+        public string partiesCarModels { get; set; }
+        public string partiesCarColor { get; set; }
+        public string partiesPartyPhone { get; set; }
+        public string partiesInsuranceCompany { get; set; }
+        public string partiesPolicyNumber { get; set; }
+        public string partiesPolicyType { get; set; }
+        public string partiesCreatedDate { get; set; }
+        public string partiesModifiedDate { get; set; }
+        public string partiesIsDeleted { get; set; }
+        public string partiesIsDeletedDate { get; set; }
+    }
+
+    public class ClaimDetailInfoModel
+    {
+        public string claimDetailEventId { get; set; }
+        public string claimDetailItem { get; set; }
+        public string claimDetailDetailid { get; set; }
+        public string claimDetailDetail { get; set; }
+        public string claimDetailLevels { get; set; }
+        public string claimDetailIsRepair { get; set; }
+        public string claimDetailRemark { get; set; }
+        public string claimDetailCreatedDate { get; set; }
+        public string claimDetailModifiedDate { get; set; }
+        public string claimDetailIsDeleted { get; set; }
+
+        public string claimDetailIsDeletedDate { get; set; }
+    }
+    
+    public class ClaimDetailPartiesInfoModel
+    {
+        public string claimDetailPartiesPartiesId { get; set; }
+        public string claimDetailPartiesItem { get; set; }
+        public string claimDetailPartiesDetailId { get; set; }
+        public string claimDetailPartiesDetail { get; set; }
+        public string claimDetailPartiesLevels { get; set; }
+        public string claimDetailPartieslIsRepair { get; set; }
+        public string claimDetailPartiesRemark { get; set; }
+        public string claimDetailPartiesCreatedDate { get; set; }
+        public string claimDetailPartiesModifiedDate { get; set; }
+        public string claimDetailPartiesIsDeleted { get; set; }
+        public string claimDetailPartiesIsDeletedDate { get; set; }
+    }
+
 }
--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/AccidentPrilimSurveyorReportController.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/AccidentPrilimSurveyorReportController.cs
@@ -9,6 +9,8 @@ using System.Web.Http;
 using Microsoft.Xrm.Sdk.Client;
 using System.Configuration;
 using Microsoft.Xrm.Tooling.Connector;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
 
 namespace DEVES.IntegrationAPI.WebApi.Controllers
 {
@@ -26,7 +28,8 @@ namespace DEVES.IntegrationAPI.WebApi.Controllers
         {
             _log.InfoFormat("IP ADDRESS: {0}, HttpMethod: POST", CommonHelper.GetIpAddress());
 
-            var output = new AccidentPrilimSurveyorReportOutputModel();
+            var outputPass = new AccidentPrilimSurveyorReportOutputModel_Pass();
+            var outputFail = new AccidentPrilimSurveyorReportOutputModel_Fail();
 
             var contentText = value.ToString();
             //_logImportantMessage = string.Format(_logImportantMessage, output.transactionId);
@@ -36,54 +39,144 @@ namespace DEVES.IntegrationAPI.WebApi.Controllers
 
             if (JsonHelper.TryValidateJson(contentText, filePath, out outvalidate))
             {
-                _logImportantMessage += "Code: " + contentModel.ticketNo;
-                output = HandleMessage(contentText, contentModel);
+                _logImportantMessage += "TicketNo: " + contentModel.ticketNo;
+                outputPass = HandleMessage(contentText, contentModel);
+
+                return Request.CreateResponse<AccidentPrilimSurveyorReportOutputModel_Pass>(outputPass);
             }
             else
             {
 
-                output = new AccidentPrilimSurveyorReportOutputModel();
+                outputFail = new AccidentPrilimSurveyorReportOutputModel_Fail();
+                outputFail.data = new AccidentPrilimSurveyorReportDataOutputModel_Fail();
+                outputFail.data.fieldErrors = new List<AccidentPrilimSurveyorReportFieldErrorsModel>();
+
+                outputFail.message = "Failed";
+                outputFail.description = "Validation json input is invalid!";
+
                 _log.Error(_logImportantMessage);
-                _log.ErrorFormat("ErrorCode: {0} {1} ErrorDescription: {1}", output.code, Environment.NewLine, output.message);
+                _log.ErrorFormat("ErrorName: {0} {1} ErrorDescription: {1}", outputFail.message, Environment.NewLine, outputFail.description);
+
+                return Request.CreateResponse<AccidentPrilimSurveyorReportOutputModel_Fail>(outputFail);
             }
-            return Request.CreateResponse<AccidentPrilimSurveyorReportOutputModel>(output);
         }
 
-        private AccidentPrilimSurveyorReportOutputModel HandleMessage(string valueText, AccidentPrilimSurveyorReportInputModel content)
+        private AccidentPrilimSurveyorReportOutputModel_Pass HandleMessage(string valueText, AccidentPrilimSurveyorReportInputModel content)
         {
             //TODO: Do what you want
-            var output = new AccidentPrilimSurveyorReportOutputModel();
-            var AccidentPrilimSurveyorReportOutput = new AccidentPrilimSurveyorReportDataOutputModel();
+            var output = new AccidentPrilimSurveyorReportOutputModel_Pass();
+            var AccidentPrilimSurveyorReportOutput = new AccidentPrilimSurveyorReportDataOutputModel_Pass();
             _log.Info("HandleMessage");
             try
             {
                 var connection = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRM_DEVES"].ConnectionString);
+
+                //OrganizationServiceProxy _serviceProxy;
+                //private Guid _accountId;
                 _serviceProxy = connection.OrganizationServiceProxy;
                 ServiceContext svcContext = new ServiceContext(_serviceProxy);
-
+                
                 try
                 {
+                    
+                    // Incident (Case)
+                    Incident incident = new Incident();
+                    incident.pfc_accident_legal_result = new OptionSetValue(Int32.Parse(convertOptionSet(Incident.EntityLogicalName, "", content.eventDetailInfo.accidentLegalResult)));
+                    incident.pfc_police_station = content.eventDetailInfo.policeStation;
+                    incident.pfc_police_record_id = content.eventDetailInfo.policeRecordId;
+                    incident.pfc_police_record_date = Convert.ToDateTime(content.eventDetailInfo.policeRecordDate);
+                    incident.pfc_police_bail_flag = convertBool(content.eventDetailInfo.policeBailFlag);
+                    incident.pfc_num_of_tow_truck = new OptionSetValue(Int32.Parse(convertOptionSet(Incident.EntityLogicalName, "", content.eventDetailInfo.numOfTowTruck.ToString())));
+                    incident.pfc_num_of_accident_injuries = new OptionSetValue(Int32.Parse(convertOptionSet(Incident.EntityLogicalName, "", content.eventDetailInfo.numOfAccidentInjury.ToString())));
+                    incident.pfc_num_of_death = new OptionSetValue(Int32.Parse(convertOptionSet(Incident.EntityLogicalName, "", content.eventDetailInfo.numOfDeath.ToString())));
+                    incident.pfc_excess_fee = content.eventDetailInfo.excessFee;
+                    incident.pfc_deductable_fee = content.eventDetailInfo.deductibleFee;
+                    incident.pfc_accident_prilim_surveyor_report_date = content.reportAccidentResultDate;
+                    // _serviceProxy.Create(incident);
+
                     // Motor Accident 
-                    pfc_motor_accident MotorAccident = new pfc_motor_accident();
-                    MotorAccident.pfc_motor_accident_name = "Napat เอง";
-                    _serviceProxy.Create(MotorAccident);
+                    pfc_motor_accident motorAccident = new pfc_motor_accident();
+                    //motorAccident.pfc_motor_accident_name = ""; // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parties
+                    //motorAccident.pfc_parent_caseId = ""; //ให้ผูกกับ Case ด้านบน ??
+                    motorAccident.pfc_activity_date = Convert.ToDateTime(content.eventDetailInfo.accidentOn);
+                    //motorAccident.pfc_event_code = ""; // ให้เป็นไปตาม Logic. ของหน้า Motor Accident
+                    motorAccident.pfc_ref_isurvey_eventid = content.eventId;
+                    motorAccident.pfc_accident_event_detail = content.eventDetailInfo.accidentNatureDesc;
+                    // motorAccident.pfc_more_document; // ไม่มี moreDocument
+                    motorAccident.pfc_accident_latitude = content.eventDetailInfo.accidentLatitude;
+                    motorAccident.pfc_accident_Longitude = content.eventDetailInfo.accidentLongitude;
+                    motorAccident.pfc_accident_location = content.eventDetailInfo.accidentPlace;
+                    motorAccident.pfc_accident_remark = content.eventDetailInfo.accidentRemark;
+                    motorAccident.pfc_ref_isurvey_total_event = content.eventDetailInfo.totalEvent;
+                    motorAccident.pfc_ref_isurvey_created_date = Convert.ToDateTime(content.eventDetailInfo.iSurveyCreatedDate);
+                    motorAccident.pfc_ref_isurvey_modified_date = Convert.ToDateTime(content.eventDetailInfo.iSurveyModifiedDate);
+                    motorAccident.pfc_ref_isurvey_isdeleted = convertBool(content.eventDetailInfo.iSurveyIsDeleted);
+                    motorAccident.pfc_ref_isurvey_isdeleted_date = Convert.ToDateTime(content.eventDetailInfo.iSurveyIsDeletedDate);
+                    motorAccident.pfc_motor_accident_parties_sum = content.eventDetailInfo.numOfAccidentParty;
+                    // _serviceProxy.Create(motorAccident);
 
-                    // Motor Accident Part
-                    //pfc_motor_accident_parts MotorAccidentPart = new pfc_motor_accident_parts();
-                    //_serviceProxy.Create(MotorAccidentParty);
-
-                    // Motor Accident Parties
-                    //pfc_motor_accident_parties MotorAccidentParties = new pfc_motor_accident_parties();
+                    // Motor Accident Parties   
+                    pfc_motor_accident_parties motorAccidentParties = new pfc_motor_accident_parties();
+                    //motorAccidentParties.pfc_ref_isurvey_partiesid
+                    //motorAccidentParties.pfc_ref_isurvey_eventid
+                    //motorAccidentParties.pfc_ref_isurvey_eventitem
+                    //motorAccidentParties.pfc_parties_fullname
+                    //motorAccidentParties.pfc_parties_type // ไม่มี field นี้
+                    //motorAccidentParties.pfc_licence_province
+                    //motorAccidentParties.pfc_licence_no
+                    //motorAccidentParties.pfc_brand
+                    //motorAccidentParties.pfc_model
+                    //motorAccidentParties.pfc_color
+                    //motorAccidentParties.pfc_phoneno
+                    //motorAccidentParties.pfc_insurance_name
+                    //motorAccidentParties.pfc_policyno
+                    //motorAccidentParties.pfc_policy_type
+                    //motorAccidentParties.pfc_ref_isurvey_created_date
+                    //motorAccidentParties.pfc_ref_isurvey_modified_date
+                    //motorAccidentParties.pfc_ref_isurvey_isdeleted
                     //_serviceProxy.Create(MotorAccidentParties);
 
+                    // Motor Accident Part
+                    pfc_motor_accident_parts motorAccidentPart = new pfc_motor_accident_parts();
+                    //motorAccidentPart.pfc_ref_isurvey_eventid
+                    //motorAccidentPart.pfc_ref_isurvey_item
+                    //motorAccidentPart.pfc_ref_isurvey_detailid
+                    //motorAccidentPart.pfc_detail
+                    //motorAccidentPart.pfc_damage_levels
+                    //motorAccidentPart.pfc_is_repair
+                    //motorAccidentPart.pfc_remark
+                    //motorAccidentPart.pfc_ref_isurvey_created_date
+                    //motorAccidentPart.pfc_ref_isurvey_modified_date
+                    //motorAccidentPart.pfc_ref_isurvey_isdeleted
+                    //motorAccidentPart.pfc_ref_isurvey_isdeleted_date
+                    //motorAccidentPart.pfc_motor_accident_event_code // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parts
+                    //motorAccidentPart.pfc_motor_accident_event_sequence // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parts
+                    //motorAccidentPart.pfc_motor_accident_parties_parts_name // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parties Parts
+                    //motorAccidentPart.pfc_motor_accident_partiesId // ให้ผูกกับ Motor Accident Parties ด้านบน
+                    //_serviceProxy.Create(motorAccidentParty);
+
                     // Motor Accident Parties Part
-                    //pfc_motor_accident_parties_parts MotorAccidentPartiesPart = new pfc_motor_accident_parties_parts();
-                    //_serviceProxy.Create(MotorAccidentPartiesPart);
+                    pfc_motor_accident_parties_parts motorAccidentPartiesPart = new pfc_motor_accident_parties_parts();
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_partiesid
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_item
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_detailid
+                    //motorAccidentPartiesPart.pfc_detail
+                    //motorAccidentPartiesPart.pfc_damage_levels
+                    //motorAccidentPartiesPart.pfc_is_repair
+                    //motorAccidentPartiesPart.pfc_remark
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_created_date
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_modified_date
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_isdeleted
+                    //motorAccidentPartiesPart.pfc_ref_isurvey_isdeleted_date
+                    //motorAccidentPartiesPart.pfc_motor_accident_event_code // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parties Parts
+                    //motorAccidentPartiesPart.pfc_motor_accident_event_sequence // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parties Parts
+                    //motorAccidentPartiesPart.pfc_motor_accident_parties_sequence // ให้เป็นไปตาม Logic. ของหน้า Motor Accident Parties Parts
+                    //_serviceProxy.Create(motorAccidentPartiesPart);
 
                 }
                 catch (Exception e)
                 {
-                    output.description = "Create Motor Accident PROBLEM";
+                    output.description = "Create Entity (Case/Motor) PROBLEM";
                     return output;
                 }
 
@@ -107,10 +200,37 @@ namespace DEVES.IntegrationAPI.WebApi.Controllers
                 _log.Error("RequestId - " + _logImportantMessage);
                 _log.Error(errorMessage);
 
-
+                output.message = e.ToString();
             }
 
             return output;
+        }
+
+        private string convertOptionSet(object entity, string fieldName, string value)
+        {
+            string valOption;
+            if (value.Length == 1)
+            {
+                valOption = "10000000" + value;
+            }
+            else
+            {
+                valOption = "1000000" + value;
+            }
+
+            return valOption;
+        }
+
+        private bool convertBool(string value)
+        {
+            if(value.Equals("Y") || value.Equals("0"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
