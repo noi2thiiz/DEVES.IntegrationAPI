102b432c05ab6cfb8c80d471adadb0b96ef76f55
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-03-31 06:59:22 +0700

    Add TransformCRMInquiryCRMPayeeListInputModel_to_SAPInquiryVendorInputModel

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformCRMInquiryCRMPayeeListInputModel_to_SAPInquiryVendorInputModel.cs
@@ -0,0 +1,25 @@
+﻿using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.APAR;
+using DEVES.IntegrationAPI.Model.InquiryCRMPayeeList;
+using DEVES.IntegrationAPI.Model.SAP;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class TransformCRMInquiryCRMPayeeListInputModel_to_SAPInquiryVendorInputModel : BaseTransformer
+    {
+        public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
+        {
+            InquiryCRMPayeeListInputModel src = (InquiryCRMPayeeListInputModel) input;
+            SAPInquiryVendorInputModel trgt = (SAPInquiryVendorInputModel) output;
+
+            trgt.PREVACC = src.polisyClientId;
+            trgt.TAX3 = src.taxNo;
+            trgt.TAX4 = src.taxBranchCode;
+            trgt.VCODE = src.sapVendorCode;
+
+
+            return output;
+        }
+    }
+}
