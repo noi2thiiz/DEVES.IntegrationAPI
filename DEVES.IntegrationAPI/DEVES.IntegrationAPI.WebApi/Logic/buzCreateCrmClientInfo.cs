0873a63b427fe6638614dcbfe82ac7bbebcf9e37
Cobb <kasemchaiw@primefactorconsulting.com>
2017-03-30 10:48:47 +0700

    buzCreateCrmClient

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzCreateCrmClientInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Newtonsoft.Json;
+
+using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.EWI;
+using DEVES.IntegrationAPI.Model.CLS;
+using DEVES.IntegrationAPI.Model.InquiryClientMaster;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class buzCreateCrmClientInfo : BaseCommand
+    {
+        public override BaseDataModel Execute(object input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
