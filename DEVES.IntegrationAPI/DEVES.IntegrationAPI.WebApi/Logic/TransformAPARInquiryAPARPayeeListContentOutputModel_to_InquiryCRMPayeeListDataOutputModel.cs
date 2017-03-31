4a52a42256ba84ff09253448934f0b7a6e8df736
Cobb <kasemchaiw@primefactorconsulting.com>
2017-03-31 19:33:12 +0700

    TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
+    {
+        public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
