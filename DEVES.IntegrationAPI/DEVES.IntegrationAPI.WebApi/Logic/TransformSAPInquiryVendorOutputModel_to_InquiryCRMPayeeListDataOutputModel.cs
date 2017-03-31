4694bc8f2f5beda1bd523410cd7debeb06b1a793
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-03-31 13:47:56 +0700

    Add TransformSAPInquiryVendorOutputModel_to_InquiryCRMPayeeListDataOutputModel

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformSAPInquiryVendorOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.SAP;
+using DEVES.IntegrationAPI.Model.InquiryClientMaster;
+using DEVES.IntegrationAPI.Model.InquiryCRMPayeeList;
+using CLS=DEVES.IntegrationAPI.Model.CLS;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class TransformSAPInquiryVendorOutputModel_to_InquiryCRMPayeeListDataOutputModel: BaseTransformer
+    {
+        public override BaseDataModel TransformModel(Model.BaseDataModel input, BaseDataModel output)
+        {
+            EWIResSAPInquiryVendorContentModel srcContent = (EWIResSAPInquiryVendorContentModel)input;
+            CRMInquiryPayeeContentOutputModel trgtContent = (CRMInquiryPayeeContentOutputModel)output;
+
+             //@todo Process Data
+
+            return trgtContent;
+        }
+    }
+}
