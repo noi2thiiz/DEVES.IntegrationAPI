8e24791b583b28355cb90844208deda5b01ab60f
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-03-31 22:56:01 +0700

    Add TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
@@ -1,17 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.APAR;
+using DEVES.IntegrationAPI.Model.InquiryCRMPayeeList;
+using DEVES.IntegrationAPI.Model.MASTER;
 using DEVES.IntegrationAPI.WebApi.Templates;
 
 namespace DEVES.IntegrationAPI.WebApi.Logic
 {
-    public class TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
+    public class
+        TransformInquiryMasterASRHContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
     {
         public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
         {
-            throw new NotImplementedException();
+            InquiryMasterASRHContentModel srcContent = (InquiryMasterASRHContentModel) input;
+            CRMInquiryPayeeContentOutputModel trgtContent = (CRMInquiryPayeeContentOutputModel) output;
+
+            trgtContent.data = new List<InquiryCrmPayeeListDataModel>();
+
+
+            foreach (var ASRHListCollection in srcContent.ASRHListCollection)
+            {
+                if (ASRHListCollection.ASRHList != null)
+                {
+                    var ASRHList = ASRHListCollection.ASRHList;
+                    trgtContent.data.Add(new InquiryCrmPayeeListDataModel
+                    {
+                        polisyClientId = ASRHList.polisyClntnum,
+                        sapVendorCode = ASRHList.vendorCode,
+                        fullName = ASRHList.fullName,
+                        taxNo = ASRHList.taxNo,
+                        taxBranchCode = ASRHList.taxBranchCode,
+                        emcsMemHeadId = ASRHList.emcsMemHeadId,
+                        emcsMemId = ASRHList.emcsMemId,
+                        address = ASRHList.address,
+                        contactNumber = ASRHList.contactNumber,
+                        assessorFlag = ASRHList.assessorFlag,
+                        solicitorFlag = ASRHList.solicitorFlag,
+                        repairerFlag = ASRHList.repairerFlag
+                        //  ASRHList "businessType": null,
+                        //  ASRHList.masterASRHCode
+                        // ASRHList.polisyClntnum
+                    });
+                }
+            }
+
+
+            return trgtContent;
         }
     }
 }
