432ccb380732ab2e112e119f7a4fc3c15e62280e
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-03-31 20:34:21 +0700

    Fix TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel.cs
@@ -1,5 +1,4 @@
-﻿<<<<<<< .mine
-using System;
+﻿using System;
 using System.Collections.Generic;
 using DEVES.IntegrationAPI.Model;
 using DEVES.IntegrationAPI.Model.APAR;
@@ -9,213 +8,44 @@ using DEVES.IntegrationAPI.WebApi.Templates;
 
 namespace DEVES.IntegrationAPI.WebApi.Logic
 {
-    public class TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
+    public class
+        TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
     {
-        private Dictionary<string, InquiryAPARPayeeContentAparPayeeListCollectionDataModel> _tmpInquiryAPARPayeeOutputModel;
         public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
         {
             InquiryAPARPayeeContentModel srcContent = (InquiryAPARPayeeContentModel) input;
             CRMInquiryPayeeContentOutputModel trgtContent = (CRMInquiryPayeeContentOutputModel) output;
 
-
-            _tmpInquiryAPARPayeeOutputModel = new Dictionary<string, InquiryAPARPayeeContentAparPayeeListCollectionDataModel>();
-            CRMInquiryPayeeContentOutputModel outputContent =
-                new CRMInquiryPayeeContentOutputModel {data = new List<InquiryCrmPayeeListDataModel>()};
-
+            trgtContent.data = new List<InquiryCrmPayeeListDataModel>();
 
 
             foreach (var aparPayeeListDataModel in srcContent.aparPayeeListCollection)
             {
                 if (aparPayeeListDataModel.aparPayeeList != null)
                 {
-                    if (!string.IsNullOrEmpty(aparPayeeListDataModel.aparPayeeList.polisyClntnum ))
+                    if (aparPayeeListDataModel.aparPayeeList != null)
                     {
-                        _tmpInquiryAPARPayeeOutputModel.Add(aparPayeeListDataModel.aparPayeeList.polisyClntnum, aparPayeeListDataModel);
+                        var aparPayeeList = aparPayeeListDataModel.aparPayeeList;
+                        trgtContent.data.Add(new InquiryCrmPayeeListDataModel
+                        {
+                            polisyClientId = aparPayeeList.polisyClntnum,
+                            sapVendorCode = aparPayeeList.vendorCode,
+                            sapVendorGroupCode = aparPayeeList.vendorGroupCode,
+
+                            address = aparPayeeList.address,
+                            telephone1 = aparPayeeList.telephone1,
+                            telephone2 = aparPayeeList.telephone2,
+
+                            faxNo = aparPayeeList.faxNo,
+
+                            taxBranchCode = aparPayeeList.taxBranchCode
+                        });
                     }
                 }
-
             }
 
-            foreach (var dataItrm in trgtContent.data)
-            {
-                if (dataItrm.polisyClientId != null &&
-                    _tmpInquiryAPARPayeeOutputModel.ContainsKey(dataItrm.polisyClientId))
-                {
-                    var _srcContentData = _tmpInquiryAPARPayeeOutputModel[dataItrm.polisyClientId];
-
-
-                    outputContent.data.Add(TransformDataModel(_srcContentData, dataItrm));
-                }
-                else
-                {
-                    outputContent.data.Add(TransformDataModel(new InquiryAPARPayeeContentAparPayeeListCollectionDataModel(), dataItrm));
-                }
-            }
-
-            return outputContent;
 
             return trgtContent;
         }
-
-        public InquiryCrmPayeeListDataModel TransformDataModel(InquiryAPARPayeeContentAparPayeeListCollectionDataModel input,
-            InquiryCrmPayeeListDataModel output)
-        {
-            if (input.aparPayeeList != null)
-            {
-
-                output.polisyClientId = input.aparPayeeList.polisyClntnum;
-                output.sapVendorCode = input.aparPayeeList.vendorCode;
-                output.sapVendorGroupCode = input.aparPayeeList.vendorGroupCode;
-                //dataItrm.emcsMemHeadId = "";
-                //dataItrm.emcsMemId = "";
-                //  output.companyCode = input.COMPANY;
-                //  output.title = input.TITLE;
-                // output.name1 = input.NAME1;
-                // output.name2 = input.NAME2;
-                // dataItrm.fullName = "";
-                // output.street1 = input.STREET1;
-                // output.street2 = input.STREET2;
-                // output.district = input.DISTRICT;
-
-                // output.city = input.CITY;
-                // output.postalCode = input.POSTCODE;
-                // output.countryCode = input.COUNTRY;
-                // output.countryCodeDesc = input.COUNTRY_DESC;
-                output.address = input.aparPayeeList.address;
-                output.telephone1 = input.aparPayeeList.telephone1;
-                output.telephone2 = input.aparPayeeList.telephone2;
-
-                output.faxNo = input.aparPayeeList.faxNo;
-                //output.contactNumber = "";
-                //  output.taxNo = input.TAX3;
-                output.taxBranchCode = input.aparPayeeList.taxBranchCode;
-
-
-                // output.paymentTerm = input.PAYTERM;
-                // output.paymentTermDesc = input.PAYTERM_DESC;
-                // output.paymentMethods = input.PAYMETHOD;
-                //output.inactive = input.INACTIVE;
-
-            }
-
-
-
-
-            return output;
-        }
-
-
-
     }
 }
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using DEVES.IntegrationAPI.Model;
-using DEVES.IntegrationAPI.WebApi.Templates;
-
-namespace DEVES.IntegrationAPI.WebApi.Logic
-{
-    public class TransformAPARInquiryAPARPayeeListContentOutputModel_to_InquiryCRMPayeeListDataOutputModel : BaseTransformer
-    {
-        public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
