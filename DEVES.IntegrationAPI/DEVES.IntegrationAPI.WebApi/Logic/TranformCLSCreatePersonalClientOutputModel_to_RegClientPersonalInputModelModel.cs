6c385aad2330eae02904c858e321086bf3867b7c
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-04-02 16:28:10 +0700

    Imprement:TranformCLSCreatePersonalClientOutputModel_to_RegClientPersonalInputModelModel

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TranformCLSCreatePersonalClientOutputModel_to_RegClientPersonalInputModelModel.cs
@@ -0,0 +1,28 @@
+﻿using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.CLS;
+using DEVES.IntegrationAPI.Model.RegClientPersonal;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class TranformCLSCreatePersonalClientOutputModel_to_RegClientPersonalInputModelModel : BaseTransformer
+    {
+        public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
+        {
+            CLSCreatePersonalClientContentOutputModel src = (CLSCreatePersonalClientContentOutputModel) input;
+            RegClientPersonalInputModel trgt =(RegClientPersonalInputModel) output;
+
+            if (src.data != null)
+            {
+
+                if (string.IsNullOrEmpty(src.data.cleansingId)) trgt.generalHeader.cleansingId = src.data.cleansingId;
+              
+
+            }
+            
+            return trgt;
+
+        }
+
+    }
+}
