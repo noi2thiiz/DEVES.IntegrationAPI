a35504018be66e49c0213649da3819743fab1cef
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-04-03 15:25:30 +0700

    Imprement:TranformInquiryCRMPayeeListInputModel_to_CLSInquiryCorporateClientInputModel TranformInquiryCRMPayeeListInputModel_to_COMPInquiryClientMasterInputModel

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/TranformInquiryCRMPayeeListInputModel_to_CLSInquiryCorporateClientInputModel.cs
@@ -0,0 +1,28 @@
+﻿using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.CLS;
+using DEVES.IntegrationAPI.Model.InquiryCRMPayeeList;
+using DEVES.IntegrationAPI.WebApi.Templates;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class TranformInquiryCRMPayeeListInputModel_to_CLSInquiryCorporateClientInputModel : BaseTransformer
+    {
+        public override BaseDataModel TransformModel(BaseDataModel input, BaseDataModel output)
+        {
+            InquiryCRMPayeeListInputModel src = (InquiryCRMPayeeListInputModel)input;
+            CLSInquiryCorporateClientInputModel trgt = (CLSInquiryCorporateClientInputModel)output;
+
+            trgt.clientId = src.polisyClientId;
+            trgt.roleCode = src.roleCode;
+
+            trgt.corporateFullName = src.fullname;
+            trgt.taxNo = src.taxNo;
+           // trgt.telephone;
+            //trgt.emailAddress;
+
+            return trgt;
+        
+        }
+
+    }
+}
