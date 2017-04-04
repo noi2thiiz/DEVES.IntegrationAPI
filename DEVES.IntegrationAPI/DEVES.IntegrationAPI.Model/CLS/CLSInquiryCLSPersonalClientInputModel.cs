b26833991e2ebbd328ff0193087128cb215e4d1f
Patiwat Wisedsukol <patiwat@tsi.in.th>
2017-04-04 17:08:08 +0700

    hardcode backDay

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/CLS/CLSInquiryCLSPersonalClientInputModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/CLS/CLSInquiryCLSPersonalClientInputModel.cs
@@ -9,11 +9,13 @@ namespace DEVES.IntegrationAPI.Model.CLS
 
     public class CLSInquiryPersonalClientInputModel : BaseDataModel
     {
-        public String clientId { set; get; }
+        public string clientId { set; get; }
         public string roleCode { set; get; }
-        public String personalFullName { set; get; }
-        public String idCitizen { set; get; }
-        public String telephone { set; get; }
-        public String emailAddress { set; get; }
+        public string personalFullName { set; get; }
+        public string idCitizen { set; get; }
+        public string telephone { set; get; }
+        public string emailAddress { set; get; }
+
+        public string backDay { set; get; }
     }
 }
