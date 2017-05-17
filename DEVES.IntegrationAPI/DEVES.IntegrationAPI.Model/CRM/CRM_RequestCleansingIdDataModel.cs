15bc7f07bc8b525f6904d766cd841441e61a6891
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-05-17 19:03:06 +0700

    update - reqclsID

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/CRM/CRM_RequestCleansingIdDataModel.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.Model/CRM/CRM_RequestCleansingIdDataModel.cs
@@ -9,9 +9,12 @@ namespace DEVES.IntegrationAPI.Model.CRM
     public class CRMRequestCleansingIdDataInputModel:BaseDataModel
     {
         public string personalCode { get; set; } = ""; // 0000 (Master Data salutation)
+        public string titlePersonalid { get; set; } = "";
+        public string titlePersonalname { get; set; } = "";
+        public string titlePersonaltype { get; set; } = "";
         public string firstname { get; set; } = "";
         public string lastname { get; set; } = "";
-        public string gendercode{ get; set; } = "";
+        public string gendercode{ get; set; } = "100000000";
         public string citizenId { get; set; } = "";
         public string telephone1{ get; set; } = "";
         public string telephone2 { get; set; } = "";
--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/RequestCleansingIdController.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Controllers/RequestCleansingIdController.cs
@@ -8,15 +8,22 @@ using DEVES.IntegrationAPI.Model.CLS;
 using DEVES.IntegrationAPI.WebApi.Templates;
 using DEVES.IntegrationAPI.Model.CRM;
 using DEVES.IntegrationAPI.WebApi.Logic.Services;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Configuration;
 
 namespace DEVES.IntegrationAPI.WebApi.Controllers
 {
     public class RequestCleansingIdController:BaseApiController
     {
 
-      
-        public IHttpActionResult RequestCleansingId(CRMRequestCleansingIdDataInputModel input)
+        OrganizationServiceProxy _serviceProxy;
+        private Guid _accountId;
+
+        public IHttpActionResult Post([FromBody]CRMRequestCleansingIdDataInputModel input)
         {
+            string clsId = "C2017-7357";
+            updateCleansingIdToCRM(input, clsId);
             if (ModelState.IsValid)
             {
 
@@ -59,24 +66,69 @@ namespace DEVES.IntegrationAPI.WebApi.Controllers
 
         }
 
-        
 
 
 
-        public void updateCleansingIdToCRM(CRMRequestCleansingIdDataInputModel input,string cleansingId)
+
+        public void updateCleansingIdToCRM(CRMRequestCleansingIdDataInputModel input, string cleansingId)
         {
-            
-              //pfc_title_personalid
-              //  firstname
-              //lastname
-              //  gendercode
-              //pfc_citizen_id
-              //  pfc_telephone1
-              //pfc_telephone2
-              //  pfc_telephone3
-              //pfc_fax
-              //  pfc_moblie_phone1
-              //emailaddress1
+            try
+            {
+                var connection = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRM_DEVES"].ConnectionString);
+
+                bool check = connection.IsReady;
+                if (!check) // check is ready = true
+                {
+                    return;// should return something that notice system about CRM is not connect properly
+                }
+
+                _serviceProxy = connection.OrganizationServiceProxy;
+                ServiceContext svcContext = new ServiceContext(_serviceProxy);
+
+                var query = from c in svcContext.ContactSet
+                            where (c.pfc_cleansing_cusormer_profile_code == null || c.pfc_cleansing_cusormer_profile_code == "")
+                        && (c.FirstName == null || c.FirstName == input.firstname)
+                        && (c.LastName == null || c.LastName == input.lastname)
+                        && (c.pfc_citizen_id == null || c.pfc_citizen_id == input.citizenId)
+                        && (c.pfc_telephone1 == null || c.pfc_telephone1 == input.telephone1)
+                        && (c.pfc_telephone2 == null || c.pfc_telephone2 == input.telephone2)
+                        && (c.pfc_telephone3 == null || c.pfc_telephone3 == input.telephone3)
+                        && (c.pfc_fax == null || c.pfc_fax == input.fax)
+                        && (c.pfc_MobilePhone1 == null || c.pfc_MobilePhone1 == input.telephone1)
+                        && (c.EMailAddress1 == null || c.EMailAddress1 == input.emailaddress1)
+                        && (c.GenderCode == null || c.GenderCode == new Microsoft.Xrm.Sdk.OptionSetValue(Int32.Parse(string.IsNullOrEmpty(input.gendercode)? "100000000": input.gendercode)))
+                        && (c.Salutation == null || c.Salutation == input.titlePersonalname)
+                            select c;
+
+                Contact personal = query.FirstOrDefault<Contact>();
+                try
+                {
+                    _accountId = new Guid(personal.Id.ToString());
+                }
+                catch (Exception e)
+                {
+                    // หาข้อมูลไม่พบ
+                }
+
+                Contact retrievedIncident = (Contact)_serviceProxy.Retrieve(Contact.EntityLogicalName, _accountId, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+                try
+                {
+                    retrievedIncident.pfc_cleansing_cusormer_profile_code = cleansingId;
+
+                    _serviceProxy.Update(retrievedIncident);
+                }
+                catch (Exception e)
+                {
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
         }
+
     }
 }
