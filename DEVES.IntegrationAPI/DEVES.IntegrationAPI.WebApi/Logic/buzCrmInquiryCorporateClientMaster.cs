2dac85cf4198e7e8418d6e484ff750d08bf98b20
Patiwat Wisedsukol <patiwat.w@hotmail.com>
2017-06-03 14:35:03 +0700

    update unit test for inq client none payee

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzCrmInquiryCorporateClientMaster.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzCrmInquiryCorporateClientMaster.cs
@@ -60,19 +60,23 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
             //ลบ บริษัทออกจากชื่อ
             if (contentModel?.conditionDetail != null)
             {
+                contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("ห้างหุ้นส่วนจำกัด", "");
                 contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("บริษัท.", "");
                 contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("บริษัท", "");
                 contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("บ.", "");
-                contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("จำกัด", "");
+               // contentModel.conditionDetail.clientFullname = contentModel?.conditionDetail?.clientFullname?.Replace("จำกัด", "");
+                
 
                 contentModel.conditionDetail.clientFullname = contentModel.conditionDetail.clientFullname
                     .ReplaceMultiplSpacesWithSingleSpace();
 
+                contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("ห้างหุ้นส่วนจำกัด", "");
+                contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName2?.Replace("ห้างหุ้นส่วนจำกัด", "");
                 contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("บริษัท.", "");
                 contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("บริษัท", "");
                 contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("บ.", "");
-                contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("จำกัด", "");
-                contentModel.conditionDetail.clientName2 = contentModel?.conditionDetail?.clientName2?.Replace("จำกัด", "");
+                //contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1?.Replace("จำกัด", "");
+                //contentModel.conditionDetail.clientName2 = contentModel?.conditionDetail?.clientName2?.Replace("จำกัด", "");
 
 
                 contentModel.conditionDetail.clientName1 = contentModel?.conditionDetail?.clientName1
@@ -200,11 +204,19 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
                 {
                     if (string.IsNullOrEmpty(data?.generalHeader?.crmClientId))
                     {
-                        List<string> crmData = SearchCrmContactClientId(contentModel?.conditionDetail?.cleansingId);
-                        if (crmData != null && crmData.Count == 1)
+                        try
                         {
-                            data.generalHeader.crmClientId = crmData.First();
+                            List<string> crmData = SearchCrmContactClientId(contentModel?.conditionDetail?.cleansingId);
+                            if (crmData != null && crmData.Count == 1)
+                            {
+                                data.generalHeader.crmClientId = crmData.First();
+                            }
                         }
+                        catch (Exception e)
+                        {
+                            AddDebugInfo("Error on search crmClientId", "Error: " + e.Message + "--" + e.StackTrace);
+                        }
+
                     }
                 }
                 #endregion Search crmClientId by CleansingId
---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApiTests1/Logic/buzCrmInquiryClientMasterTests.cs
@@ -0,0 +1,480 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DEVES.IntegrationAPI.WebApi.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.InquiryClientMaster;
+using DEVES.IntegrationAPI.Model.InquiryCRMPayeeList;
+using DEVES.IntegrationAPI.WebApi.Templates.Exceptions;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic.Tests
+{
+    [TestClass()]
+    public class buzCrmInquiryClientMasterTests
+    {
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMasterTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType="P",
+                        roleCode="G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                       polisyClientId = "14290379"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("14290379", ewiResponse.data[0].generalHeader.polisyClientId);
+               // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByFullnameTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        clientFullname = "พรชัย สว่างรุ่งเรือง"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("14290379", ewiResponse.data[0].generalHeader.polisyClientId);
+                Assert.AreEqual("พรชัย สว่างรุ่งเรือง", ewiResponse.data[0].profileInfo.fullName);
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByFirstNameAndLastNameTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        clientName1 = "พรชัย",clientName2 = "สว่างรุ่งเรือง"
+                     
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("14290379", ewiResponse.data[0].generalHeader.polisyClientId);
+                Assert.AreEqual("พรชัย สว่างรุ่งเรือง", ewiResponse.data[0].profileInfo.fullName);
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchBySubFullnameTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        clientFullname = "พร"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual(true, ewiResponse.data.Count>20);
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchBySubFullname_NotFoundTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        clientFullname = "พรชัยชัยกรรม กรร"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(false, ewiResponse.data.Any());
+               
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByPolisyClientId_NotFoundTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        polisyClientId = "9999"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(false, ewiResponse.data.Any());
+
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByIdCardTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "P",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        idCard = "3500900949465"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("13372519", ewiResponse.data[0].generalHeader.polisyClientId);
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByIdCard_Type_C_Test()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "C",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        idCard = "0107559000371"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("0107559000371", ewiResponse.data[0].profileInfo.idTax);
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByFullname_Type_C_Test()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "C",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                       clientFullname = "ห้างหุ้นส่วนจำกัด บี พรชัยการค้า"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("15814599", ewiResponse.data[0].generalHeader.polisyClientId);
+                Assert.AreEqual("0307334800149", ewiResponse.data[0].profileInfo.idTax);
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByPolisyClientId_Type_C_Test()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "C",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        polisyClientId = "17364357"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(true, ewiResponse.data.Any());
+                Assert.AreEqual("17364357", ewiResponse.data[0].generalHeader.polisyClientId);
+
+                // Assert.AreEqual("CLS", ewiResponse.data[0].sourceData);
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void Execute_buzCrmInquiryClientMaster_SearchByPolisyClientId_Type_C_NotfoundTest()
+        {
+            AppConfig.Instance.StartupForUnitTest();
+            try
+            {
+                var cmd = new buzCrmInquiryClientMaster();
+                var input = new InquiryClientMasterInputModel
+                {
+                    conditionHeader = new ConditionHeaderModel
+                    {
+                        clientType = "C",
+                        roleCode = "G"
+                    },
+                    conditionDetail = new ConditionDetailModel
+                    {
+                        polisyClientId = "179999357"
+                    }
+                };
+                var result = cmd.Execute(input);
+                Console.WriteLine("==============result=============");
+                Console.WriteLine(result.ToJson());
+                Assert.IsNotNull(result);
+                var ewiResponse = (CRMInquiryClientContentOutputModel)result;
+
+
+                Assert.AreEqual("200", ewiResponse.code);
+                Assert.AreEqual(false, ewiResponse.data.Any());
+        
+
+            }
+            catch (BuzErrorException e)
+            {
+
+                Assert.Fail();
+            }
+
+
+        }
+    }
+}
