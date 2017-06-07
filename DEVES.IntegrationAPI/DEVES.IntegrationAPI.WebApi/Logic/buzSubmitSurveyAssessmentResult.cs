5c64ab531b78bba7d597ccef5345af2af0b7c023
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-06-07 11:42:06 +0700

    update -> buzSubmitSurveyAssessment

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzSubmitSurveyAssessmentResult.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzSubmitSurveyAssessmentResult.cs
@@ -8,6 +8,7 @@ using DEVES.IntegrationAPI.Model.SubmitSurveyAssessmentResult;
 using Microsoft.Xrm.Tooling.Connector;
 using System.Configuration;
 using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk;
 
 namespace DEVES.IntegrationAPI.WebApi.Logic
 {
@@ -35,6 +36,24 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
             // Condition check if ref_code don't have in CRM -> UPDATE
             if(query.FirstOrDefault<pfc_assessment>() == null)
             {
+                pfc_assessment create = new pfc_assessment();
+
+                // assessment
+                create.pfc_assessment_claim_noti_date = DateTime.Now;
+                create.pfc_assessment_claim_noti_score = contentModel.assessmentClaimNotiScore;
+                create.pfc_assessment_claim_noti_comment = contentModel.assessmentClaimNotiComment;
+                create.pfc_assessment_survey_date = DateTime.Now;
+                create.pfc_assessment_survey_score = contentModel.assessmentSurveyScore;
+                // create.pfc_assessment_survey_speed_score = contentModel.assessmentSurveySpeedScore;
+                create.pfc_assessment_survey_comment = contentModel.assessmentSurveyComment;
+                create.pfc_assessment_claim_noti_by = string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid) ? new OptionSetValue(100000000) : new OptionSetValue(100000001);
+                create.pfc_assessment_survey_by = string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid) ? new OptionSetValue(100000000) : new OptionSetValue(100000001);
+                create.pfc_assessment_claim_noti_by_userid = new EntityReference(pfc_motor_accident_parties.EntityLogicalName, "pfc_assessment_claim_noti_by_userid", contentModel.assessmentSurveyByUserid);
+                create.pfc_assessment_survey_by_userid = new EntityReference(pfc_motor_accident_parties.EntityLogicalName, "pfc_assessment_survey_by_userid", contentModel.assessmentSurveyByUserid);
+
+
+                _serviceProxy.Create(create);
+
                 // WAITING FOR MAPPING DOCUMENT
                 output.code = AppConst.CODE_SUCCESS;
                 output.message = "รอ LOGIC จากพี่ไกด์";
@@ -47,9 +66,11 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
             // if already have ref_code in CRM -> RETURN message
             else 
             {
+                pfc_assessment firstQuery = query.FirstOrDefault<pfc_assessment>();
+
                 output.code = AppConst.CODE_SUCCESS;
                 output.message = "ได้ทำการตอบแบบสอบถามนี้แล้ว";
-                output.description = "ref_code ที่กรอกมีอยู่แล้วในระบบ CRM";
+                output.description = "ref_code: " + firstQuery.pfc_assessment_ref_code + " มีอยู่แล้วในระบบ CRM";
                 output.transactionId = TransactionId;
                 output.transactionDateTime = DateTime.Now.ToString();
 
