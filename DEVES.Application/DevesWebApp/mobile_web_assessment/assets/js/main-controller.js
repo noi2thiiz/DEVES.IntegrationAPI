dfee8048e0c8739023d31d740726215cfa4ca83a
Patiwat Wisedsukol <patiwat.w@hotmail.com>
2017-06-09 04:49:34 +0700

    Complete Web Assessment Mobile

--- DEVES.Application/DevesWebApp/mobile_web_assessment/assets/js/main-controller.js
+++ DEVES.Application/DevesWebApp/mobile_web_assessment/assets/js/main-controller.js
@@ -24,27 +24,42 @@ app.run(['$window', '$location', '$loading', '$http', '$cookies',function ($wind
 
 }]);
 app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','$cookies','$location', function ($scope, dialog, $loading, $http,$q,$cookies,$location) {
-    var vm = this;
-    vm.deferred = $q.defer();
-    vm.deferred.resolve();
+   //debugger;
+    //var vm = this;
+    //vm.deferred = $q.defer();
+    //vm.deferred.resolve();
 
     // $("#main-page").attr('disabled',true);
 
 
     var refCode = $.trim(getParameterByName("ref"));
-    if(!refCode){
-        refCode="";
+
+    if(refCode.length!=11){
+        dialog.alert({
+            title: "Error",
+            content: "ขออภัยรหัสอ้างอิงแบบประเมินนี้ไม่ถูกต้อง"
+        });
     }
 
-    var usuerGid = $.trim(getParameterByName("ref"));
-    if(!usuerGid){
-        usuerGid="";
+    var usuerGid = $.trim(getParameterByName("uid"));
+    if(usuerGid==""){
+        usuerGid = $.trim(getParameterByName("guid"));
     }
-
+    if(usuerGid==""){
+        usuerGid = $.trim(getParameterByName("userid"));
+    }
+    if(usuerGid==""){
+        usuerGid = $.trim(getParameterByName("userGuid"));
+    }
+   
 
     var ref = refCode.substr(0,10);
 
     var assessmentType = Number(refCode.substr(10,1));
+    //set default
+    if($.trim(assessmentType)==""){
+        assessmentType = 1;
+    }
 
     var assessmentSurveyByUserid = "";
     var assessmentGarageByUserid = "";
@@ -63,78 +78,272 @@ app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','
     }
 
 
+
     var queryString =  $location.search();
     console.log(queryString);
     var scoreData = {};
-    $scope.questions = [
-        {
-            id:1,
-            title:" ข้อ 1/3",
-            group:1,
+    $scope.questions=[];
 
-            subQuestions: [
-                {id:1, group:2,
-                    ref:'assessmentClaimNotiScore',
-                    title: "การบริการของเจ้าหน้าที่รับแจ้งอุบัติเหตุ มีความพึงพอใจระดับใด",
-                    value:0}
+    if(assessmentType==2) {
 
-           ]
-        },
-        {
-            id:2,
-            title:" ข้อ 2/3",
-            group:2,
+        $scope.questions = [
+            {
+                id: 1,
+                title: " ข้อ 1/3",
+                group: 1,
 
-            subQuestions: [
-                {id:1, ref:'assessmentSurveyScore',
-                    group:2,title: "การเดินทางถึงที่เกิดเหตุของเจ้าหน้าที่สำรวจภัย มีความพึงพอใจระดับใด",value:0}
+                subQuestions: [
+                    {
+                        id: 1, group: 2,
+                        ref: 'assessmentGarageServiceScore',
+                        title: "การให้บริการและการต้อนรับของอู่",
+                        value: 0
+                    }
 
-            ]
-        },
-        {
-            id:3,
-            title:"ข้อ 3/3",
-            group:3,
+                ]
+            },
+            {
+                id: 2,
+                title: " ข้อ 2/3",
+                group: 2,
 
-            subQuestions: [
-                {id:1,group:3,
-                    ref:'assessmentSurveySpeedScore',
-                    title: "การบริการและความรวดเร็วในการทำงานของเจ้าหน้าที่สำรวจภัย มีความพึงพอใจระดับใด" ,value:0}
-            ]
-        },
+                subQuestions: [
+                    {
+                        id: 1, ref: 'assessmentGarageCommitScore',
+                        group: 2, title: "ระยะเวลาที่ใช้ในการจัดซ่อมเหมาะสม", value: 0
+                    }
 
+                ]
+            },
+            {
+                id: 3,
+                title: "ข้อ 3/3",
+                group: 3,
 
-    ];
-
-
-
-   function showPage(pageId) {
-
-       $("section").hide();
-       $( "#" +pageId).animate({
-           opacity: "show"
-       }, {
-           duration: 500
-       });
-
-       if(history.pushState) {
-           history.pushState(null, null, "#"+pageId);
-       }
-       else {
-           location.hash = '#'+pageId;
-       }
-   }
+                subQuestions: [
+                    {
+                        id: 1, group: 3,
+                        ref: 'assessmentGarageRepairScore',
+                        title: "ความเรียบร้อยของงานซ่อมและความสะอาดตอนส่งมอบได้มาตรฐาน", value: 0
+                    }
+                ]
+            }
 
 
+        ];
+
+    }else {
+        $scope.questions = [
+            {
+                id: 1,
+                title: " ข้อ 1/3",
+                group: 1,
+
+                subQuestions: [
+                    {
+                        id: 1, group: 2,
+                        ref: 'assessmentClaimNotiScore',
+                        title: "การบริการของเจ้าหน้าที่รับแจ้งอุบัติเหตุ มีความพึงพอใจระดับใด",
+                        value: 0
+                    }
+
+                ]
+            },
+            {
+                id: 2,
+                title: " ข้อ 2/3",
+                group: 2,
+
+                subQuestions: [
+                    {
+                        id: 1, ref: 'assessmentSurveyScore',
+                        group: 2, title: "การเดินทางถึงที่เกิดเหตุของเจ้าหน้าที่สำรวจภัย มีความพึงพอใจระดับใด", value: 0
+                    }
+
+                ]
+            },
+            {
+                id: 3,
+                title: "ข้อ 3/3",
+                group: 3,
+
+                subQuestions: [
+                    {
+                        id: 1, group: 3,
+                        ref: 'assessmentSurveySpeedScore',
+                        title: "การบริการและความรวดเร็วในการทำงานของเจ้าหน้าที่สำรวจภัย มีความพึงพอใจระดับใด", value: 0
+                    }
+                ]
+            }
 
 
+        ];
+    }
+
+
+    var currentScreenWidth = "";
+    var currentScreenHeight = "";
+    function resize() {
+        $("body").css("visibility","hidden");
+    if(    currentScreenWidth !=  screen.width
+        || currentScreenHeight != screen.height
+        ){
+        currentScreenWidth=screen.width
+        currentScreenHeight=screen.height
+        //alert("resize"+currentScreenWidth+":"+currentScreenHeight);
+
+
+        if($("body").width()>400){
+            $("#home-img-full").show();
+            $("#home-img-mobile").hide();
+
+            $("#home-page .page-header").hide();
+            $("#page-footer").hide();
+            //$("#thanks-page-body").height(screen.height-200);
+
+            //  $("#thanks-page-body-link").css("margin-top",$("#thanks-page-body").height()-100);
+
+
+
+        }else{
+            $("#home-img-full").hide();
+            $("#home-img-mobile").show();
+
+            $("home-page .page-header").show();
+            $("#page-footer").show();
+            //  $("#thanks-page-body").height($("body").innerHeight()-250);
+
+        }
+
+        $("#thanks-page-body,#thanks-page-body-left-panel,#thanks-page-body-right-panel").css({
+            "height": "95%"
+        });
+
+
+
+
+        if(window.innerHeight<=500){
+            $("#home-page .page-header").hide();
+            $("#textarea-comment").attr("rows",5);
+            $("#page-footer").hide();
+        }
+        var bodyHeight = window.innerHeight;
+        var bodywidth = $("#thanks-page-body-left-panel").innerWidth();
+
+        var spaceHeight =  bodyHeight-$(".vertical-center").innerHeight();
+        $(".vertical-center").css("margin-top",spaceHeight/4);
+
+        $(".deves-icon").height((bodywidth*0.9)/3);
+
+
+
+        setTimeout(function() {
+
+            var rightWidth = $("#thanks-page-body-right-panel").width();
+
+            $("#img-deves-line").css("margin-top",bodyHeight/4);
+            $("#img-deves-line").height("auto");
+            $("#img-deves-line").width(rightWidth*0.95);
+
+            if(window.innerHeight<=150) {
+                $("#img-thanks-img").height("50px").width("auto");
+                $(".deves-icon").height((bodywidth*0.8)/3.8);
+
+            }else if(window.innerHeight<=300){
+
+
+
+
+                $("#img-deves-logo").height("10px");
+
+                $("#img-thanks-img").height("50px");
+                $(".deves-icon").height((bodywidth*0.8)/3);
+
+
+                $("#img-thanks-img").height("90px").width("auto");
+                $(".deves-icon").height((bodywidth*0.6)/3);
+
+                $("#img-deves-line").css("margin-top","30px");
+                $("#img-deves-line").width(rightWidth*0.7);
+                $("#img-deves-logo").height("20px");
+                $("#img-deves-socail").height("20px").width("auto").css("bottom","2px").css("right","2px");
+                $("#deves-icon-container").css("margin-top","0px");
+
+            }
+            else if(window.innerHeight<=500){
+                $("#img-thanks-img").height("120px").width("auto");
+                $("#img-deves-socail").height("50px").width("auto").css("bottom","2px").css("right","2px");
+
+            }
+            else{
+                $("#img-deves-socail").height("auto").width("90%").css("bottom","2px").css("right","2px");
+
+
+
+
+                if(window.innerHeight < window.innerWidth){
+                    $("#img-thanks-img").width(bodywidth*0.6).height("auto");
+                }else{
+                    $("#img-thanks-img").width(bodywidth*0.9).height("auto");
+                }
+                var spaceHeight =  bodyHeight-$(".vertical-center").innerHeight();
+                $(".vertical-center").css("margin-top",spaceHeight/4);
+
+                $(".deves-icon").height((bodywidth*0.9)/3);
+                if(bodyHeight-$(".vertical-center").innerHeight()>150){
+                    //alert(bodyHeight-$(".vertical-center").innerHeight());
+
+
+                    $("#deves-icon-container").css("margin-top","50px");
+                }
+
+            }
+        }, 200);
+
+
+
+    }
+        $("body").css("visibility","visible");
+    }
+/*
+    var supportsOrientationChange = "onorientationchange" in window,
+        orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+    window.addEventListener(orientationEvent, function() {
+        alert('HOLY ROTATING SCREENS BATMAN:' + window.orientation + " " + screen.width);
+    }, false);
+*/
+    function showPage(pageId) {
+
+        $("section").hide();
+        $( "#" +pageId).animate({
+            opacity: "show"
+        }, {
+            duration: 500
+        });
+
+        if(history.pushState) {
+            history.pushState(null, null, "#"+pageId);
+        }
+        else {
+            location.hash = '#'+pageId;
+        }
+
+        if(pageId=="thanks-page"){
+           $(".page-header").remove();
+            $("#page-footer").remove();
+            $("body").css("background",'url("img/deves/left_panel_bg.png")')
+
+        }
+        resize();
+    }
 
 
     $scope.currentQuestionId = 0;
     $scope.currentSubQuestionId = 0;
 
-    $scope.currentQuestion;
-    $scope.currentSubQuestion;
+    $scope.currentQuestion=null;
+    $scope.currentSubQuestion=null;
 
     $scope.selectQuestion = function (main,sub) {
         $loading.start('options');
@@ -194,15 +403,29 @@ app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','
         }
 
 
-    }
+    };
 
     $scope.startQuestion = function () {
+        try{
+            $("#home-page").hide();
+            showPage("question-page");
 
-        $("#home-page").hide();
-        showPage("question-page");
-        $("#page-footer").show();
-        $scope.selectQuestion(0,0);
-    }
+            if(window.innerHeight<=500){
+                $("#home-page .page-header").hide();
+                $("#textarea-comment").attr("rows",5);
+                $("#page-footer").hide();
+            }else{
+                $("#page-footer").show();
+            }
+
+            $scope.selectQuestion(0,0);
+        }catch (e){
+            console.warn(e)
+
+        }
+
+    };
+
 
     $scope.comment = "";
     $scope.submitQuestionResult  = function(){
@@ -273,6 +496,7 @@ app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','
                     //success
                     $cookies.put('assessmentStatus_'+refCode,"complete");
                     showPage("thanks-page");
+                    location.href = "";
 
 
                 } else {
@@ -323,79 +547,24 @@ app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','
 
     }
     $("textarea").on("focus",function () {
-        $("#page-footer").hide();
-        $(".rcorners1").height(50);
-
-    })
-    $("textarea").on("blur",function () {
-        $("#page-footer").show();
-        $(".rcorners1").height(100);
-    })
-
-
-    function resize() {
-
-         $("body").append("<div id='screenInfo' class='screenInfo'></div>");
-         $("#screenInfo").text(window.innerWidth+"x"+window.innerHeight);
-        //alert(($("body").height()));
-
-       // $("#thanks-page-body").height(screen.height-(screen.height*0.4));
-        //$("#thanks-page-body-left-panel").height($("#thanks-page-body").height());
-        //$("#thanks-page-body-right-panel").height($("#thanks-page-body").height());
-
-
-            if($("body").width()>400){
-                $("#home-img-full").show();
-                $("#home-img-mobile").hide();
-
-                $("#home-page .page-header").hide();
-                $("#page-footer").hide();
-                //$("#thanks-page-body").height(screen.height-200);
-
-              //  $("#thanks-page-body-link").css("margin-top",$("#thanks-page-body").height()-100);
-
-
-
-            }else{
-                $("#home-img-full").hide();
-                $("#home-img-mobile").show();
-
-                $("home-page .page-header").show();
-                $("#page-footer").show();
-              //  $("#thanks-page-body").height($("body").innerHeight()-250);
-
-            }
-
-        if(window.innerHeight < window.innerWidth){
-
-            //$("#thanks-page-body-link").css("margin-top",$("#thanks-page-body").height()-60);
-
-            $("#thanks-page-body,#thanks-page-body-left-panel,#thanks-page-body-right-panel").css({
-                "height": "95%"
-            });
-            $("#img-thanks-img").height(100);
-            $("#img-thanks-img").width(100);
-            document.getElementById("img-deves-line").style.marginTop = "10px !important";
-            $("#img-deves-line").css("cssText", "margin-top: 100px !important;");
-            //$("#img-deves-line").css('margin-top',"10px","!important");
-
-
-           // $("#thanks-page-body-link img").height(80);
-            //$("#thanks-page-body-link img").width(80);
-
-        }else{
-            //$("#thanks-page-body-link").css("margin-top",$("#thanks-page-body").height()-100);
-
-            //$("#thanks-page-body-link img").height(80);
-            //$("#thanks-page-body-link img").width(80);
+        if(window.innerHeight<500) {
+            $("#page-footer").hide();
+            $(".rcorners1").height(50);
         }
 
+    });
+
+    $("textarea").on("blur",function () {
+        if(window.innerHeight>=500){
+            $("#page-footer").show();
+            $(".rcorners1").height(100);
+        }
+
+    });
 
 
 
-    }
-    $(window).resize(resize);
-    resize();
+
 /*
     if(window.location.hash) {
         var hash = window.location.hash.substr(1);
@@ -411,6 +580,14 @@ app.controller('mainController', ['$scope', 'dialog', '$loading', '$http','$q','
     };
 
 
+    $( document ).ready(function() {
+        console.log( "ready!" );
+        $(window).on("load", function() {
+            $(window).resize(resize);
+            resize();
+        });
 
+
+    });
 
 }]);
