b406ebf457cc1326d8050fb1b6752d4eff33988c
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-06-09 17:38:27 +0700

    assessment + XRM

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzSubmitSurveyAssessmentResult.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzSubmitSurveyAssessmentResult.cs
@@ -31,8 +31,9 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
             OrganizationServiceProxy _serviceProxy = connection.OrganizationServiceProxy;
             ServiceContext svcContext = new ServiceContext(_serviceProxy);
 
+            int type = 100000000 + contentModel.assessmentType - 1;
             var query = from c in svcContext.pfc_assessmentSet
-                        where c.pfc_assessment_ref_code == contentModel.assessmentrefcode
+                        where c.pfc_assessment_ref_code == contentModel.assessmentrefcode && c.pfc_assessment_type.Value == type
                         select c;
 
 
@@ -50,54 +51,58 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
             // if already have ref_code in CRM -> RETURN message
             else
             {
-
                 pfc_assessment firstQuery = query.FirstOrDefault<pfc_assessment>();
-                // Guid guid = new Guid(firstQuery.Id.ToString());
-                // pfc_assessment retrievedAssessment = (pfc_assessment)_serviceProxy.Retrieve(pfc_assessment.EntityLogicalName, guid, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+                Guid guid = new Guid(firstQuery.Id.ToString());
+                pfc_assessment retrievedAssessment = (pfc_assessment)_serviceProxy.Retrieve(pfc_assessment.EntityLogicalName, guid, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
 
-                // if Type = 1 (Survey)
-                if (contentModel.assessmentType == 1)
+                if (firstQuery.pfc_assessment_survey_status.Value != 100000000)
                 {
-                    // check that survey assessment was done??
-                    // ทำการประเมินแล้ว
-                    if (firstQuery.pfc_assessment_survey_status.Value == 100000001)
+                    output.code = AppConst.CODE_FAILED;
+                    output.message = "ไม่สามารถบันทึกคะแนนได้";
+                    output.description = "สถานะไม่ถูกต้อง: (" + firstQuery.pfc_assessment_survey_status.Value + ")";
+                    output.transactionId = TransactionId;
+                    output.transactionDateTime = DateTime.Now.ToString();
+
+                    return output;
+                }
+                else
+                {
+                    var queryQuestionnair = from c in svcContext.pfc_questionnairSet
+                                            where c.pfc_questionnairId == contentModel.assessmentquestionnireid
+                                            select c;
+
+                    if (queryQuestionnair.FirstOrDefault<pfc_questionnair>() == null)
                     {
                         output.code = AppConst.CODE_FAILED;
                         output.message = "ไม่สามารถบันทึกคะแนนได้";
-                        output.description = "เนื่องจากมีการบันทึกคะแนนไปแล้ว จึงไม่สามารถบันทึกคะแนนซ้ำได้";
+                        output.description = "ไม่สามารถบันทึกคะแนนได้เนื่องจากไม่พบแบบสำรวจความพึงพอใจ";
                         output.transactionId = TransactionId;
                         output.transactionDateTime = DateTime.Now.ToString();
 
                         return output;
                     }
-                    // ยังไม่ได้ทำการประเมิน
                     else
                     {
-                        Guid guid = new Guid(firstQuery.Id.ToString());
-                        pfc_assessment retrievedAssessment = (pfc_assessment)_serviceProxy.Retrieve(pfc_assessment.EntityLogicalName, guid, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
-
-                        // survey assessment
-                        retrievedAssessment.pfc_assessment_claim_noti_date = DateTime.Now;
-                        retrievedAssessment.pfc_assessment_claim_noti_score = contentModel.assessmentClaimNotiScore;
-                        retrievedAssessment.pfc_assessment_claim_noti_comment = contentModel.assessmentClaimNotiComment;
-                        retrievedAssessment.pfc_assessment_survey_date = DateTime.Now;
-                        retrievedAssessment.pfc_assessment_survey_score = contentModel.assessmentSurveyScore;
-                        retrievedAssessment.pfc_assessment_survey_speed_score = contentModel.assessmentSurveySpeedScore;
-                        retrievedAssessment.pfc_assessment_survey_timeusage_score = contentModel.assessmentSurveyTimeUsageScore;
-                        retrievedAssessment.pfc_assessment_survey_comment = contentModel.assessmentSurveyComment;
+                        retrievedAssessment.pfc_assessment_date = DateTime.Now;
+                        retrievedAssessment.pfc_assessment_score1 = contentModel.assessmentScore1;
+                        retrievedAssessment.pfc_assessment_score2 = contentModel.assessmentScore2;
+                        retrievedAssessment.pfc_assessment_score3 = contentModel.assessmentScore3;
+                        retrievedAssessment.pfc_assessment_score4 = contentModel.assessmentScore4;
+                        retrievedAssessment.pfc_assessment_score5 = contentModel.assessmentScore5;
+                        retrievedAssessment.pfc_assessment_score6 = contentModel.assessmentScore6;
+                        retrievedAssessment.pfc_assessment_score7 = contentModel.assessmentScore7;
+                        retrievedAssessment.pfc_assessment_score8 = contentModel.assessmentScore8;
+                        retrievedAssessment.pfc_assessment_score9 = contentModel.assessmentScore9;
+                        retrievedAssessment.pfc_assessment_score10 = contentModel.assessmentScore10;
+                        retrievedAssessment.pfc_assessment_comment = contentModel.assessmentComment;
                         retrievedAssessment.pfc_assessment_survey_status = new OptionSetValue(100000001);
-                        retrievedAssessment.pfc_assessment_claim_noti_by = string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid) ? new OptionSetValue(100000000) : new OptionSetValue(100000001);
-                        retrievedAssessment.pfc_assessment_survey_by = string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid) ? new OptionSetValue(100000000) : new OptionSetValue(100000001);
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid)) { 
-                            retrievedAssessment.pfc_assessment_claim_noti_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentSurveyByUserid));
-                        }
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid))
+                        if (retrievedAssessment.pfc_assessment_by_userid == null)
                         {
-                            retrievedAssessment.pfc_assessment_survey_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentSurveyByUserid));
+                            retrievedAssessment.pfc_assessment_by = new OptionSetValue(100000000);
                         }
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid))
+                        else
                         {
-                            retrievedAssessment.pfc_assessment_garage_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentGarageByUserid));
+                            retrievedAssessment.pfc_assessment_by = new OptionSetValue(100000001);
                         }
                         retrievedAssessment.pfc_assessment_survey_ipaddress = HttpContext.Current.Request.UserHostAddress;
 
@@ -122,84 +127,8 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
                         output.transactionDateTime = DateTime.Now.ToString();
                         return output;
                     }
+
                 }
-                // if Type = 2 (Garage)
-                else if (contentModel.assessmentType == 2)
-                {
-                    // check that survey assessment was done??
-                    // ทำการประเมินแล้ว
-                    if (firstQuery.pfc_assessment_garage_status.Value == 100000001)
-                    {
-                        output.code = AppConst.CODE_FAILED;
-                        output.message = "ไม่สามารถบันทึกคะแนนได้";
-                        output.description = "เนื่องจากมีการบันทึกคะแนนไปแล้ว จึงไม่สามารถบันทึกคะแนนซ้ำได้";
-                        output.transactionId = TransactionId;
-                        output.transactionDateTime = DateTime.Now.ToString();
-
-                        return output;
-                    }
-                    // ยังไม่ได้ทำการประเมิน
-                    else
-                    {
-                        Guid guid = new Guid(firstQuery.Id.ToString());
-                        pfc_assessment retrievedAssessment = (pfc_assessment)_serviceProxy.Retrieve(pfc_assessment.EntityLogicalName, guid, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
-
-                        // garage assessment
-                        retrievedAssessment.pfc_assessment_garage_date = DateTime.Now;
-                        retrievedAssessment.pfc_assessment_garage_service_score = contentModel.assessmentGarageServiceScore;
-                        retrievedAssessment.pfc_assessment_garage_commit_score = contentModel.assessmentGarageCommitScore;
-                        retrievedAssessment.pfc_assessment_garage_repair_score = contentModel.assessmentGarageRepairScore;
-                        retrievedAssessment.pfc_assessment_garage_comment = contentModel.assessmentGarageComment;
-                        retrievedAssessment.pfc_assessment_garage_status = new OptionSetValue(100000001);
-                        retrievedAssessment.pfc_assessment_garage_by = string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid) ? new OptionSetValue(100000000) : new OptionSetValue(100000001);
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid))
-                        {
-                            retrievedAssessment.pfc_assessment_claim_noti_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentSurveyByUserid));
-                        }
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid))
-                        {
-                            retrievedAssessment.pfc_assessment_survey_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentSurveyByUserid));
-                        }
-                        if (!string.IsNullOrEmpty(contentModel.assessmentSurveyByUserid))
-                        {
-                            retrievedAssessment.pfc_assessment_garage_by_userid = new EntityReference(pfc_assessment.EntityLogicalName, new Guid(contentModel.assessmentGarageByUserid));
-                        }
-                        retrievedAssessment.pfc_assessment_garage_ipaddress = HttpContext.Current.Request.UserHostAddress;
-
-                        try
-                        {
-                            _serviceProxy.Update(retrievedAssessment);
-                        }
-                        catch (Exception e)
-                        {
-                            output.code = AppConst.CODE_FAILED;
-                            output.message = "ไม่สามารถบันทึกคะแนนได้";
-                            output.description = e.Message;
-                            output.transactionId = TransactionId;
-                            output.transactionDateTime = DateTime.Now.ToString();
-                            return output;
-                        }
-
-                        output.code = AppConst.CODE_SUCCESS;
-                        output.message = AppConst.MESSAGE_SUCCESS;
-                        output.description = "บันทึกคะแนนเรียบร้อยแล้ว";
-                        output.transactionId = TransactionId;
-                        output.transactionDateTime = DateTime.Now.ToString();
-                        return output;
-                    }
-                }
-                // CASE assessmentType != 1 and 2
-                else
-                {
-                    output.code = AppConst.CODE_FAILED;
-                    output.message = "ไม่สามารถบันทึกคะแนนได้";
-                    output.description = "assessmentType ไม่ใช่สถานะทั้ง Survey และ Garage";
-                    output.transactionId = TransactionId;
-                    output.transactionDateTime = DateTime.Now.ToString();
-
-                    return output;
-                }
-
             }
 
         }
