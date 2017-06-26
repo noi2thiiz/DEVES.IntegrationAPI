4d54ce23be584a4e2e55b5f36a3c74d336bcaebd
Patiwat Wisedsukol <patiwat.w@hotmail.com>
2017-06-26 13:32:22 +0700

    update assessment guid config

--- DEVES.Application/DevesWebApp/mobile_web_assessment/assets/js/config.js
+++ DEVES.Application/DevesWebApp/mobile_web_assessment/assets/js/config.js
@@ -1,7 +1,26 @@
-window.appConfig = {
-    assessmentQuestionnaire : [
-        "",
-        "D181A7B8-FA4C-E711-80DA-0050568D615F", /*1= surveyGuid*/
-        "BDA64EC4-FA4C-E711-80DA-0050568D615F" /*2= garageGuid*/
-    ]
-};
+var hostname = window.location.hostname;
+switch(hostname){
+
+    case "csat-qa.deves.co.th":
+        window.appConfig = {
+            assessmentQuestionnaire : [
+                "",
+                "D181A7B8-FA4C-E711-80DA-0050568D615F", /*1= surveyGuid*/
+                "BDA64EC4-FA4C-E711-80DA-0050568D615F" /*2= garageGuid*/
+            ]
+        };
+        break;
+
+    default:
+        window.appConfig = {
+            assessmentQuestionnaire : [
+                "",
+                "FB59FF14-3E57-E711-80CB-0050568D6908", /*1= surveyGuid*/
+                "FD59FF14-3E57-E711-80CB-0050568D6908" /*2= garageGuid*/
+            ]
+        };
+        break;
+}
+
+
+
