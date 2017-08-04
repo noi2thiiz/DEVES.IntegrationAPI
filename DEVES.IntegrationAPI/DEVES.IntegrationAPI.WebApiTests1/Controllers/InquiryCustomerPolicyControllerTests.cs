cfdfe448ab888e24c8237a13150fa7641fc0572d
Patiwat Wisedsukol <patiwat.w@hotmail.com>
2017-08-04 11:26:06 +0700

    add unit test example

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApiTests1/Controllers/InquiryCustomerPolicyControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace DEVES.IntegrationAPI.WebApi.Controllers.Tests
+{
+    [TestClass()]
+    public class InquiryCustomerPolicyControllerTests : TestControllerBase
+    {
+        /**
+         * ทดสอบกรณีที่ ค้นแล้วพบข้อมูล ด้วย cleansingId
+         */
+        [TestMethod()]
+        public void Post_InquiryCustomerCase_It_Should_Success_When_Give_Valid_CleansingIdTest()
+        {
+
+            string input = @"
+            {
+              'generalHeader': {
+                'requester': 'WEB'
+              },
+              'conditions': {
+                'cleansingId': 'C2017-005367960',
+                'crmClientId': '',
+                'policyCarRegisterNo': '',
+                'policyNo': '',
+                'chassisNo': ''
+              }
+            }";
+
+            var output = PostMessage("InquiryCustomerCase", input);
+            Console.WriteLine(output);
+
+            //Assert
+            var outputJson = JObject.Parse(output);
+            Assert.AreEqual("200", outputJson["code"]?.ToString());
+            Assert.AreEqual(false, string.IsNullOrEmpty(outputJson["transactionId"]?.ToString()));
+            Assert.AreEqual(false, string.IsNullOrEmpty(outputJson["transactionDateTime"]?.ToString()));
+            Assert.AreEqual(false, string.IsNullOrEmpty(outputJson["message"]?.ToString()));
+
+
+
+
+            // ทดสอบว่า data มีค่า
+        
+            Assert.IsNotNull(outputJson["data"],"data is null");
+          
+            // ทดสอบว่า data รายการแรก มี ค่าที่เป็น Mandatory  
+
+            // var outputData = outputJson["data"][0];
+           // Assert.AreEqual(false, string.IsNullOrEmpty(outputData["generalHeader"]["polisyClientId"]?.ToString()));
+            // Assert.AreEqual(false, string.IsNullOrEmpty(outputData["generalHeader"]["cleansingId"]?.ToString()));
+
+
+
+        }
+
+
+
+        /**
+      * ทดสอบกรณีที่ ค้นแล้วพบข้อมูล ด้วย crmClientId
+      */
+        [TestMethod()]
+        public void Post_InquiryCustomerCase_It_Should_Success_When_Give_Valid_crmClientIdTest()
+        {
+        }
+
+        /**
+         * ทดสอบกรณีที่ ใส่ json  input ไม่ครบ ที่ไม่มีอยู่จริง
+         * ให้ทดสอบโดยไม่ระบุ  cleansingId
+         */
+        [TestMethod()]
+        public void Post_InquiryCustomerCase_It_Should_Invalid_Input_Test()
+        {
+        }
+
+        /**
+         * ทดสอบกรณีที่ ระบุ cleansingId ที่ไม่มีอยู่จริง
+         */
+        [TestMethod()]
+        public void Post_InquiryCustomerCase_It_Should_NotFound_When_Give_Invalid_CleansingId_Test()
+        {
+        }
+
+        /**
+         * ทดสอบกรณีที่ ระบุ crmClientId ที่ไม่มีอยู่จริง
+         */
+        [TestMethod()]
+        public void Post_InquiryCustomerCase_It_Should_NotFound_When_Give_Invalid_crmClientId_Test()
+        {
+        }
+    }
+}
---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApiTests1/Controllers/TestControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using DEVES.IntegrationAPI.WebApi.Core.DataAdepter;
+
+namespace DEVES.IntegrationAPI.WebApi.Controllers.Tests
+{
+    public class TestControllerBase
+    {
+        protected string PostMessage(string apiName, string json)
+        {
+
+            string endpoint = "https://crmappdev.deves.co.th/proxy/xml.ashx?http://192.168.8.121/inquiryAPI/api/" + apiName;
+            var client = new RESTClient(endpoint);
+            var result = client.Execute(json);
+
+            var jss = new JavaScriptSerializer();
+
+            return result.Content;
+        }
+    }
+}
