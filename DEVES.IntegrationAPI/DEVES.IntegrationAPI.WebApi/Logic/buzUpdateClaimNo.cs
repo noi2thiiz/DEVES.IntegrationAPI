8fce76f69345272adc8b51821e3994d029898d1d
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-08-09 10:42:50 +0700

    refactor

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/buzUpdateClaimNo.cs
@@ -0,0 +1,35 @@
+﻿using DEVES.IntegrationAPI.WebApi.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DEVES.IntegrationAPI.Model;
+using DEVES.IntegrationAPI.Model.UpdateClaimNo;
+using System.Configuration;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace DEVES.IntegrationAPI.WebApi.Logic
+{
+    public class buzUpdateClaimNo : BuzCommand
+    {
+        public override BaseDataModel ExecuteInput(object input)
+        {
+            // Deserialize Input
+            UpdateClaimNoInputModel contentInput = (UpdateClaimNoInputModel)input;
+
+            // Preparation Variable
+            UpdateClaimNoOutputModel_Pass output = new UpdateClaimNoOutputModel_Pass();
+
+            // Preparation Linq query to CRM
+            var connection = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRM_DEVES"].ConnectionString);
+            OrganizationServiceProxy _serviceProxy = connection.OrganizationServiceProxy;
+            ServiceContext svcContext = new ServiceContext(_serviceProxy);
+
+            var queryCase = from c in svcContext.IncidentSet
+                            where c.TicketNumber == contentInput.ticketNo
+                            select c;
+
+        }
+    }
+}
