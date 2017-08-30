1a76347bc2ccaaef0d7c8a1a8046c6d913b51453
Patiwat Wisedsukol <patiwat.w@hotmail.com>
2017-08-30 10:52:39 +0700

    Complete DVS-P1-0001 Client Issue

--- DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/Modules/Client/buzCRMRegClientCorporate.cs
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApi/Logic/Modules/Client/buzCRMRegClientCorporate.cs
@@ -98,11 +98,7 @@ namespace DEVES.IntegrationAPI.WebApi.Logic
                 regClientCorporateInput?.addressHeader?.addressType
             );
 
-            //"addressHeader.addressType"
-            regClientCorporateInput.addressHeader.addressType = validator.TryConvertAddressTypeCode(
-                "addressInfo.addressType",
-                regClientCorporateInput?.addressHeader?.addressType
-            );
+        
 
             //"profileHeader.econActivity"
             regClientCorporateInput.profileHeader.econActivity = validator.TryConvertEconActivityCode(
