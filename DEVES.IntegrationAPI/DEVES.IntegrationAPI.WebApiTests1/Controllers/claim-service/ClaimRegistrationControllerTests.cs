305fd055b7ba9e6cd90c8c2737c1e36c8ee9b032
np_jaa <np_jaa@DESKTOP-4188VJJ>
2017-09-11 18:35:29 +0700

    unit test for claim and isurvey service

---
+++ DEVES.IntegrationAPI/DEVES.IntegrationAPI.WebApiTests1/Controllers/claim-service/ClaimRegistrationControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using DEVES.IntegrationAPI.WebApi.Controllers;
+using DEVES.IntegrationAPI.WebApi.Controllers.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVES.IntegrationAPI.WebApiTests1.Controllers.claim_service
+{
+    [TestClass]
+    public class ClaimRegistrationControllerTests : BaseControllersTests
+    {
+        [TestMethod]
+        public void Post_ClaimRegistrationController_It_Should_Fail_When_Give_Duplicate_Input_Test()
+        {
+            //input
+            var jsonString = @"
+                {  
+                   ""incidentId"":""DC8C0ADA-4A91-E711-80DA-0050568D615F"",
+                   ""currentUserId"":""50529F88-2BE9-E611-80D4-0050568D1874""
+                }
+            ";
+
+            // ระบุ  ที่ต้องการทดสอบ และ Method ที่ต้องการทดสอบ ในตัวอย่างนี้ต้องการ  ทดสอบ Method  Post  
+            var response = ExcecuteControllers<ClaimRegistrationController>(jsonString, "Post");
+            Console.WriteLine("==============output==================");
+            Assert.IsNotNull(response?.Result);
+            Console.WriteLine(response?.Result);
+
+            //แปลง string เป็น JObject
+            var outputJson = JObject.Parse(response?.Result);
+            // Assert Return code 200
+            Assert.AreEqual("claimNotiNo is duplicated", outputJson["errorMessage"]?.ToString());
+        }
+    }
+}
